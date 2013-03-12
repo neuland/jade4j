cb83ea4a4da96f9a4b87c1e3a713dddac0494958
Dhaval <dhaval.doshi@gmail.com>
2013-03-12 17:14:49 +0530

    Dhaval/ashish12 | added support for if conditional with block in mixins

--- src/test/java/de/neuland/jade4j/template/JadeRunFullTemplateTest.java
+++ src/test/java/de/neuland/jade4j/template/JadeRunFullTemplateTest.java
@@ -20,30 +20,43 @@ public class JadeRunFullTemplateTest {
 	
 	@Test
 	public void testFullRun() throws IOException {
-		JadeConfiguration cfg = new JadeConfiguration();
-		//	cfg.setDirectoryForTemplateLoading(new File("/where/you/store/templates"));
-		Map<String, Object> root = new HashMap<String, Object>();
-		root.put("hello", "world");
-		root.put("hallo", null);
-		JadeModel model = new JadeModel(root);
-		
-		JadeTemplate temp = cfg.getTemplate(getResourcePath("fullrun"));
+        String templateName = "fullrun";
+        String expected = "<div><div>Hi everybody</div></div>";
+        assertTemplateRendering(templateName, expected);
+	}
+
+	@Test
+	public void testFullRunForMixinWithConditional() throws IOException {
+        String templateName = "mixin_with_conditional";
+        String expected = "<div class=\"wrapper conditional\"><label>another nested label</label></div>";
+        assertTemplateRendering(templateName, expected);
+	}
+
+    private void assertTemplateRendering(String templateName, String expected) throws IOException {
+        JadeConfiguration cfg = new JadeConfiguration();
+        //	cfg.setDirectoryForTemplateLoading(new File("/where/you/store/templates"));
+        Map<String, Object> root = new HashMap<String, Object>();
+        root.put("hello", "world");
+        root.put("hallo", null);
+        JadeModel model = new JadeModel(root);
+
+        JadeTemplate temp = cfg.getTemplate(getResourcePath(templateName));
 
 //		Writer out = new OutputStreamWriter(System.err);
-		StringWriter out = new StringWriter();
-		try {
+        StringWriter out = new StringWriter();
+        try {
             temp.process(model, out);
         } catch (JadeCompilerException e) {
             e.printStackTrace();
             fail();
         }
-		out.flush();
-		assertEquals("<div><div>Hi everybody</div></div>", out.toString());
-	}
-	
+        out.flush();
+        assertEquals(expected, out.toString());
+    }
+
     public String getResourcePath(String fileName) {
     	try {
-			return TestFileHelper.getRootResourcePath() + "/template/fullrun";
+			return TestFileHelper.getRootResourcePath() + "/template/" + fileName;
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
