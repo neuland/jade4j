277a5fd66dd9283db9d81e49a1ffe0cbc125e43e
Zatkovský Dušan <Dusan.Zatkovsky@ysoft.com>
2015-02-11 16:53:47 +0100

    added compiler tests for 'comma bug' in 'mixin' node

--- src/test/java/de/neuland/jade4j/compiler/CompilerTest.java
+++ src/test/java/de/neuland/jade4j/compiler/CompilerTest.java
@@ -317,6 +317,16 @@ public class CompilerTest {
     }
 
     @Test
+    public void mixinWithCommaSinglearg() {
+        run("mixin_with_comma_singlearg");
+    }
+
+    @Test
+    public void mixinWithCommaMorearg() {
+        run("mixin_with_comma_morearg");
+    }
+
+    @Test
     public void largeBodyTextWithPipes() {
         // TODO add missing newline
         run("large_body_text_with_pipes");
