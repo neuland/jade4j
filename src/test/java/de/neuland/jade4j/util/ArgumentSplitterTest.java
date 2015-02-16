767c0870f2476a1328c888ee7e5a419f2b6d63ac
Martin Hvizdos <martin.hvizdos@ysoft.com>
2015-02-16 14:53:15 +0100

    added tests that test splitting arguments with nested function calls

--- src/test/java/de/neuland/jade4j/util/ArgumentSplitterTest.java
+++ src/test/java/de/neuland/jade4j/util/ArgumentSplitterTest.java
@@ -1,8 +1,11 @@
 package de.neuland.jade4j.util;
 
-import org.junit.Test;
-import java.util.List;
+import static de.neuland.jade4j.util.ArgumentSplitter.split;
 import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
 
 /**
  * @author dusan.zatkovsky, 2/9/15
@@ -13,54 +16,63 @@ public class ArgumentSplitterTest  {
     public void testSplit() throws Exception {
 
         List<String> args;
-        args = ArgumentSplitter.split("foo.faa('this is arg1'),'this is arg2'");
+        args = split("foo.faa('this is arg1'),'this is arg2'");
         assertEquals(args.size(), 2);
         assertEquals( "foo.faa('this is arg1')", args.get(0));
         assertEquals( "'this is arg2'", args.get(1));
 
-        args = ArgumentSplitter.split("foo.faa ( 'this is arg1'), 'this is arg2'");
+        args = split("foo.faa ( 'this is arg1'), 'this is arg2'");
         assertEquals(args.size(), 2);
         assertEquals( "foo.faa ( 'this is arg1')", args.get(0));
         assertEquals( "'this is arg2'", args.get(1));
 
-        args = ArgumentSplitter.split("foo.faa(\"this is arg1\"),\"this is arg2\"");
+        args = split("foo.faa(\"this is arg1\"),\"this is arg2\"");
         assertEquals(args.size(), 2);
         assertEquals( "foo.faa(\"this is arg1\")", args.get(0));
         assertEquals( "\"this is arg2\"", args.get(1));
 
-        args = ArgumentSplitter.split("foo.faa ( \"this is arg1\" ) , \"this is arg2\" ");
+        args = split("foo.faa ( \"this is arg1\" ) , \"this is arg2\" ");
         assertEquals(args.size(), 2);
         assertEquals( "foo.faa ( \"this is arg1\" )", args.get(0));
         assertEquals( "\"this is arg2\"", args.get(1));
 
-        args = ArgumentSplitter.split("1");
+        args = split("1");
         assertEquals(args.size(), 1);
         assertEquals( "1", args.get(0));
 
-        args = ArgumentSplitter.split("1,2,3");
+        args = split("1,2,3");
         assertEquals(args.size(), 3);
         assertEquals( "1", args.get(0));
         assertEquals( "2", args.get(1));
         assertEquals( "3", args.get(2));
 
-        args = ArgumentSplitter.split("1 , 2, 3");
+        args = split("1 , 2, 3");
         assertEquals(args.size(), 3);
         assertEquals( "1", args.get(0));
         assertEquals( "2", args.get(1));
         assertEquals( "3", args.get(2));
 
-        args = ArgumentSplitter.split("1 , '2', 3");
+        args = split("1 , '2', 3");
         assertEquals(args.size(), 3);
         assertEquals( "1", args.get(0));
         assertEquals( "'2'", args.get(1));
         assertEquals( "3", args.get(2));
 
-        args = ArgumentSplitter.split("'1' , '2', '3'");
+        args = split("'1' , '2', '3'");
         assertEquals(args.size(), 3);
         assertEquals( "'1'", args.get(0));
         assertEquals( "'2'", args.get(1));
         assertEquals( "'3'", args.get(2));
 
+        args = split("bar(bazz, 'bazz'), 123");
+        assertEquals(args.size(), 2);
+        assertEquals( "bar(bazz, 'bazz')", args.get(0));
+        assertEquals( "123", args.get(1));
 
+        args = split("123, '1,2,3', \"a,b,c\"");
+        assertEquals(args.size(), 3);
+        assertEquals( "123", args.get(0));
+        assertEquals( "'1,2,3'", args.get(1));
+        assertEquals( "\"a,b,c\"", args.get(2));
     }
 }
