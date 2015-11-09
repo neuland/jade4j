01697082600029168a06102df4f9d0d6a96ee56e
Christoph Blömer <christoph.bloemer@neuland-bfi.de>
2015-11-09 17:01:12 +0100

    Issue 107: Strange filter behavior

--- src/test/java/de/neuland/jade4j/TestFileHelper.java
+++ src/test/java/de/neuland/jade4j/TestFileHelper.java
@@ -11,6 +11,7 @@ public class TestFileHelper {
     public static final String TESTFILE_ORIGINAL_FOLDER = "/originalTests/";
     public static final String TESTFILE_20150927_ORIGINAL_FOLDER = "/originalTests20150927/";
     public static final String TESTFILE_20150515_ORIGINAL_FOLDER = "/originalTests20150515/";
+    public static final String TESTFILE_ISSUES_FOLDER = "/issues/";
     public static final String TESTFILE_COMPILER_ERROR_FOLDER = "/compiler/errors/";
     
 	public static String getResourcePath(String fileName) throws FileNotFoundException {
@@ -67,6 +68,14 @@ public class TestFileHelper {
 	public static String getOriginal20150515ResourcePath(String fileName) {
 		try {
 			return getResourcePath(TESTFILE_20150515_ORIGINAL_FOLDER + fileName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static String getIssuesResourcePath(String fileName) {
+		try {
+			return getResourcePath(TESTFILE_ISSUES_FOLDER + fileName);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
---
+++ src/test/java/de/neuland/jade4j/compiler/IssuesTest.java
@@ -0,0 +1,81 @@
+package de.neuland.jade4j.compiler;
+
+import de.neuland.jade4j.Jade4J;
+import de.neuland.jade4j.JadeConfiguration;
+import de.neuland.jade4j.TestFileHelper;
+import de.neuland.jade4j.filter.*;
+import de.neuland.jade4j.template.FileTemplateLoader;
+import de.neuland.jade4j.template.JadeTemplate;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IssuesTest {
+    private static String[] ignoredCases = new String[]{"attrs"};
+
+    private String file;
+
+    public IssuesTest(String file) {
+        this.file = file;
+    }
+
+    @Test
+    public void shouldCompileJadeToHtml() throws Exception {
+        JadeConfiguration jade = new JadeConfiguration();
+        jade.setTemplateLoader(new FileTemplateLoader(TestFileHelper.getIssuesResourcePath(""),"UTF-8"));
+//        jade.setExpressionHandler(new JsExpressionHandler());
+        jade.setMode(Jade4J.Mode.XHTML); // original jade uses xhtml by default
+        jade.setFilter("plain", new PlainFilter());
+        jade.setFilter("cdata", new CDATAFilter());
+        jade.setFilter("custom-filter", new CustomTestFilter());
+        jade.setFilter("marked", new MarkdownFilter());
+        jade.setFilter("verbatim", new VerbatimFilter());
+        jade.setFilter("js", new JsFilter());
+        jade.setFilter("css", new CssFilter());
+//        jade.setFilter("coffee-script", new CoffeeScriptFilter());
+
+        jade.setPrettyPrint(true);
+        JadeTemplate template = jade.getTemplate(file);
+        Writer writer = new StringWriter();
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("title","Jade");
+        jade.renderTemplate(template,model, writer);
+        String html = writer.toString();
+
+        String expected = readFile(file.replace(".jade", ".html")).trim().replaceAll("\r", "");
+
+        assertEquals(file, expected, html.trim());
+    }
+
+    private String readFile(String fileName) throws IOException {
+        return FileUtils.readFileToString(new File(TestFileHelper.getIssuesResourcePath(fileName)));
+    }
+
+    @Parameterized.Parameters(name="{0}")
+    public static Collection<String[]> data() {
+        File folder = new File(TestFileHelper.getIssuesResourcePath(""));
+        Collection<File> files = FileUtils.listFiles(folder, new String[]{"jade"}, false);
+
+        Collection<String[]> data = new ArrayList<String[]>();
+        for (File file : files) {
+            if (!ArrayUtils.contains(ignoredCases, file.getName().replace(".jade", ""))) {
+                data.add(new String[]{file.getName()});
+            }
+
+        }
+        return data;
+    }
+}
