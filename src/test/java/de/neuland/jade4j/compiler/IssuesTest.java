0fb2eb14bdbf855567c5de96a6ec3e7386d44a6c
Christoph Blömer <christoph.bloemer@neuland-bfi.de>
2015-11-10 08:45:07 +0100

    TestCase for Issue 101: "}" symbol breaks the template inside of a string

--- src/test/java/de/neuland/jade4j/compiler/IssuesTest.java
+++ src/test/java/de/neuland/jade4j/compiler/IssuesTest.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class IssuesTest {
-    private static String[] ignoredCases = new String[]{"104b_mixin","layout"};
+    private static String[] ignoredCases = new String[]{"104b_mixin","layout","101"};
 
     private String file;
 
