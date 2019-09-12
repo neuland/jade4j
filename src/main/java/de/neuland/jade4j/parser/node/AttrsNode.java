877e9a3e09384b938069f23c4f2963b74f2f2e18
steffengiese <steffen.giese@neuland-bfi.de>
2019-09-12 17:02:43 +0200

    fix vars.pug

--- src/main/java/de/neuland/jade4j/parser/node/AttrsNode.java
+++ src/main/java/de/neuland/jade4j/parser/node/AttrsNode.java
@@ -175,10 +175,10 @@ public abstract class AttrsNode extends Node {
             }
         }
         LinkedHashMap<String,String> finalAttributes = new LinkedHashMap<String,String>();
-        finalAttributes.putAll(newAttributes);
         if(!classes.isEmpty()){
             finalAttributes.put("class", StringUtils.join(classes," "));
         }
+        finalAttributes.putAll(newAttributes);
         return finalAttributes;
     }
 
