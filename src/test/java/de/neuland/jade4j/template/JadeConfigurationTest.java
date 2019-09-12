892026facb2d8ffc1d732791de23be0b059c5faf
Fabian Grote <fabian.grote@neuland-bfi.de>
2019-09-12 10:45:29 +0200

    refactor for readability

--- src/test/java/de/neuland/jade4j/template/JadeConfigurationTest.java
+++ src/test/java/de/neuland/jade4j/template/JadeConfigurationTest.java
@@ -6,7 +6,6 @@ import de.neuland.jade4j.parser.Parser;
 import de.neuland.jade4j.parser.node.Node;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.Assert.*;
@@ -15,59 +14,53 @@ public class JadeConfigurationTest {
 
     protected Parser parser;
     protected Node root;
-    
+
+    private final String templateLocation = TestFileHelper.getParserResourcePath("assignment.jade");
+
     @Test
-	public void testGetTemplate() throws IOException {
-    	JadeConfiguration config = new JadeConfiguration();
-    	JadeTemplate template = config.getTemplate(getParserResourcePath("assignment"));
-    	assertNotNull(template);
+    public void testGetTemplate() throws IOException {
+        JadeConfiguration config = new JadeConfiguration();
+        JadeTemplate template = config.getTemplate(templateLocation);
+        assertNotNull(template);
     }
 
     @Test
     public void testCache() throws IOException {
         JadeConfiguration config = new JadeConfiguration();
         config.setCaching(true);
-        JadeTemplate template = config.getTemplate(getParserResourcePath("assignment"));
+        JadeTemplate template = config.getTemplate(templateLocation);
         assertNotNull(template);
-        JadeTemplate template2 = config.getTemplate(getParserResourcePath("assignment"));
+        JadeTemplate template2 = config.getTemplate(templateLocation);
         assertNotNull(template2);
         assertSame(template, template2);
     }
 
     @Test
-	public void testExceptionOnUnknowwTemplate() throws IOException {
-    	JadeConfiguration config = new JadeConfiguration();
-    	JadeTemplate template = null;
-    	try {
-    		template = config.getTemplate("UNKNOWN_PATH");
-    		fail("Did expect TemplatException!");
-    	} catch (IOException ignore) {
-    		
-    	}
-    	assertNull(template);
+    public void testExceptionOnUnknowwTemplate() throws IOException {
+        JadeConfiguration config = new JadeConfiguration();
+        JadeTemplate template = null;
+        try {
+            template = config.getTemplate("UNKNOWN_PATH");
+            fail("Did expect TemplatException!");
+        } catch (IOException ignore) {
+
+        }
+        assertNull(template);
     }
-    
+
     @Test
     public void testPrettyPrint() throws IOException {
-    	JadeConfiguration config = new JadeConfiguration();
-    	config.setPrettyPrint(true);
-    	JadeTemplate template = config.getTemplate(getParserResourcePath("assignment"));
-    	assertTrue(template.isPrettyPrint());
+        JadeConfiguration config = new JadeConfiguration();
+        config.setPrettyPrint(true);
+        JadeTemplate template = config.getTemplate(templateLocation);
+        assertTrue(template.isPrettyPrint());
     }
 
     @Test
     public void testRootNode() throws IOException {
-    	JadeConfiguration config = new JadeConfiguration();
-    	JadeTemplate template = config.getTemplate(getParserResourcePath("assignment"));
-    	assertNotNull(template.getRootNode());
+        JadeConfiguration config = new JadeConfiguration();
+        JadeTemplate template = config.getTemplate(templateLocation);
+        assertNotNull(template.getRootNode());
     }
-    
-    public String getParserResourcePath(String fileName) {
-    	try {
-			return TestFileHelper.getRootResourcePath() + "/parser/" + fileName;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-    	return null;
-    }
+
 }
