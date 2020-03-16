45c8438f2444ac5fd46cdadfa10602db88a5c0bb
Christoph Blömer <christoph.bloemer@neuland-bfi.de>
2020-03-16 19:26:01 +0100

    Fixing issue #193: Mixin's block argument can't execute multiple times in a loop

--- src/main/java/de/neuland/pug4j/parser/node/MixinBlockNode.java
+++ src/main/java/de/neuland/pug4j/parser/node/MixinBlockNode.java
@@ -5,11 +5,16 @@ import de.neuland.pug4j.exceptions.PugCompilerException;
 import de.neuland.pug4j.model.PugModel;
 import de.neuland.pug4j.template.PugTemplate;
 
+import java.util.LinkedList;
+
 public class MixinBlockNode extends Node {
     @Override
     public void execute(IndentWriter writer, PugModel model, PugTemplate template) throws PugCompilerException {
-        Node node = pollNode();
-        if(node != null)
-            node.execute(writer, model, template);
+        LinkedList<Node> nodes = getNodes();
+        if(nodes.size()==1) {
+            Node node = nodes.get(0);
+            if (node != null)
+                node.execute(writer, model, template);
+        }
     }
 }
